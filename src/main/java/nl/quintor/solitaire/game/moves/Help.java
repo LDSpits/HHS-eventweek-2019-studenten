ddb495d2750dc1ca332dc15bd3e106d23643ebc6
Lucas Spits <spits.lucas@gmail.com>
2019-02-01 11:35:50 +0100

    Update Help message to be according to tests

--- src/main/java/nl/quintor/solitaire/game/moves/Help.java
+++ src/main/java/nl/quintor/solitaire/game/moves/Help.java
@@ -16,8 +16,22 @@ public class Help implements Move{
      */
     @Override
     public String apply(GameState gameState) {
-        // TODO: Write implementation
-        return "Solitaire game\nThe following options are available:\n";
+        String help =
+            "You can give a command with the Capital letter followed by <Return>,\n" +
+            "so in order to display these instructions, simply type \"H + <Return>\"!\n" +
+            "You can simply hit <Return> to repeat the last command you entered.\n" +
+            "The H̲elp command requires arguments. The syntax is: \n" +
+            " \n" +
+            "M Source Destination (case insensitive)\n" +
+            " \n" +
+            "For example:\n" +
+            "\"M O SA\" moves the top card from the Stock to the top of Stack Pile A\n" +
+            "\"M SB F\" moves the top card from Stack Pile B to the end of Column F\n" +
+            " \n" +
+            "Dutch Patience rules: http://www.patiencespel.nl/patiencespelregels.php\n" +
+            "English Patience rules: http://digsolitaire.com/solitaire-rules.php\n";
+
+        return help;
     }
 
     @Override
@@ -27,6 +41,6 @@ public class Help implements Move{
 
     @Override
     public String toString() {
-        return "Help";
+        return "H̲elp";
     }
 }
